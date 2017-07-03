b39ff9b53023e23eeedb7aeeca56f73f30e5e0ff
Keven Kurz <kkurz@ctl.local>
2017-07-03 14:00:51 +0200

    rewrite

--- index.js
+++ index.js
@@ -8,15 +8,31 @@ const WunderlistSDK = require('wunderlist');
 const wunderlistAPI = new WunderlistSDK(tokenID);
 
 
-const { lists, notes, tasks } = wunderlistAPI.http;
+const {
+  lists,
+  notes,
+  tasks,
+} = wunderlistAPI.http;
 
-
-lists.getID(308101424).then(data => console.log(data));
-
-const logNote = taskID => notes.forTask(taskID).then(n => n[0] && console.log(` -${n[0].content}`));
-
-
-tasks.forList(308101424).then(ts => ts.forEach((t) => {
-  console.log(t.title);
-  logNote(t.id);
+const getNewest = (acc, l) => {
+  if (!acc) return l;
+  const lTime = Date.parse(l.created_at).getTime();
+  const accTime = Date.parse(acc.created_at).getTime();
+  return lTime < accTime ? l : acc;
+};
+const mapNotes = t => notes.forTask(t.id).then(ns => Object.assign(t, {
+  notes: ns,
 }));
+
+lists.all().done(ls => 4).then(console.log);
+
+// Promise.all(lists.all()).then(ls => Promise.resolve(4)).then(console.log).catch(console.warn);
+// .then((ls) => {
+//   const workLists = ls.filter(l => l.title.toLowerCase() === 'work');
+//   const lastWorkList = workLists.reduce(getNewest);
+//   return tasks.forList(lastWorkList.id);
+// }).then(console.log);
+
+
+// .then(id => ).then(console.log);
+// ts => Promise.all(ts.map(mapNotes)))
