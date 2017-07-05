048acc55cf09947f181dd09444b8ddc8f13f120a
Keven Kurz <kkurz@ctl.local>
2017-07-05 16:46:45 +0200

    script opens outlook and creates new email

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-/* eslint-disable no-alert, no-console */
-
-
+/* eslint-disable no-alert, no-console, linebreak-style, no-useless-escape */
+import {
+  execFile,
+} from 'child_process';
+import encodeUrl from 'encodeurl';
 import tokenID from './tokenID';
 
 const WunderlistSDK = require('wunderlist');
@@ -18,7 +20,7 @@ const {
 const getLists = () => new Promise((resolve, reject) => lists.all().done(resolve).fail(reject));
 const getList = id => new Promise((resolve, reject) => lists.getID(id).done(resolve).fail(reject));
 const createList = obj =>
-    new Promise((resolve, reject) => lists.create(obj).done(resolve).fail(reject));
+  new Promise((resolve, reject) => lists.create(obj).done(resolve).fail(reject));
 
 const getTasks = (id, completed = false) => new Promise((resolve, reject) =>
   tasks.forList(id, completed).done(resolve).fail(reject));
@@ -36,40 +38,49 @@ const getLastWorkID = (ls) => {
   const lastWorkList = workLists.reduce(getNewest);
   return lastWorkList.id;
 };
-const writeTask = (t) => {
-  console.log(t.title);
-  t.notes.forEach(n => console.log(` -${n.content}`));
+const writeTask = (acc, t) => {
+  let ret = acc;
+  ret += `${t.title}\n`;
+  ret += t.notes.reduce((acc2, n) => `${acc2} -${n.content}\n`, '');
+  return ret;
 };
 const writeTasks = (ts) => {
-  console.log('Aufgaben erledigt und offen\n');
-  console.log('Offen:');
-  ts[0].forEach(writeTask);
-  console.log('\nErledigt:');
-  ts[1].forEach(writeTask);
+  let msg = '';
+  msg += 'werkstattleitung@ctl.de?subject=Aufgaben erledigt und offen';
+  msg += '&body=Offen:\n';
+  msg += ts[0].reduce(writeTask, '');
+  msg += '\n\nErledigt:\n';
+  msg += ts[1].reduce(writeTask, '');
+  return encodeUrl(msg);
 };
-const addNote = t => getNotes(t.id).then(n => ({ ...t, notes: n }));
+const addNote = t => getNotes(t.id).then(n => ({ ...t,
+  notes: n,
+}));
 const addNotes = taskArray => Promise.all(taskArray.map(addNote));
 
 const getAllTasks = id =>
   Promise.all([getTasks(id).then(addNotes), getTasks(id, true).then(addNotes)]);
 
+const openOutlook = (msg) => {
+  execFile('C:\\Program Files (x86)\\Microsoft Office\\Office16\\OUTLOOK.exe', ['/c', 'ipm.note', '/m', msg], () => process.exit());
+};
 
 const args = process.argv.slice(2);
-
-if (args[0].toLocaleString() === 'report') {
-  getLists()
-  .then(getLastWorkID)
-  .then(getAllTasks)
-  .then(writeTasks)
-  .then(() => process.exit());
-} else if (args[0].toLocaleString() === 'new') {
-  getLists()
-  .then(getLastWorkID)
-  .then(id => getList(id))
-  .then(console.log);
-  // TODO createList here
+if (!args[0]) {
+  console.log('test');
   process.exit();
-} else {
-  console.log('help text');
+}
+if (args[0] && args[0].toLocaleString() === 'report') {
+  getLists()
+    .then(getLastWorkID)
+    .then(getAllTasks)
+    .then(writeTasks)
+    .then(openOutlook);
+} else if (args[0] && args[0].toLocaleString() === 'new') {
+  getLists()
+    .then(getLastWorkID)
+    .then(id => getList(id))
+    .then(console.log);
+  // TODO createList here
   process.exit();
 }
